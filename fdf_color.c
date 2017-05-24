5aad5dd394009172b02e01ead7980b0f45de5675
rmbozhko <rm.bozhko@gmail.com>
2017-05-24 15:13:08 +0300

    colors normed

--- fdf_color.c
+++ fdf_color.c
@@ -1,169 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf_color.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbozhko <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/24 14:19:46 by rbozhko           #+#    #+#             */
+/*   Updated: 2017/05/24 14:21:32 by rbozhko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fdf.h"
 
-long int 	fdf_get_pnt_distance_grad(t_pnt *pnt1, t_pnt *pnt2, int error2)
+static	int		get_middle_color(int times)
 {
-	t_brezenheim	b;
-	
-	b.last_pnt = pnt2;
-	b.x = (int)pnt1->x;
-	b.y = (int)pnt1->y;
-	b.last_pnt = pnt2;
-	b.del_x = abs((int)b.last_pnt->x - b.x);
-    b.del_y = abs((int)b.last_pnt->y - b.y);
-    b.sign_x = b.x < (int)b.last_pnt->x ? 1 : -1;
-    b.sign_y = b.y < (int)b.last_pnt->y ? 1 : -1;
-    b.error = b.del_x - b.del_y;
-    b.counter = 0;
-    while(b.x != (int)b.last_pnt->x || b.y != (int)b.last_pnt->y) 
-   {
-        error2 = b.error * 2;
-        if(error2 > -b.del_y) 
-        {
-            b.error -= b.del_y;
-            b.x += b.sign_x;
-            b.counter++;        
-        }
-        if(error2 < b.del_x) 
-        {
-            b.error += b.del_x;
-            b.y += b.sign_y;
-            b.counter++;
-        }
-    }
-    return (b.counter);
+	int		color;
+
+	if (times == 0)
+		color = 98;
+	else if (times == 1)
+		color = 50;
+	else if (times == 2)
+		color = 45;
+	return (color);
 }
 
-int		fdf_pxl_color(int main_color)
+static int		get_minor_color(int times)
 {
-	
 	int		color;
-	static int times = 0;
-	if (main_color == 0)
-	{
-		if (times == 0)
-		{
-			color = 80;
-			times++;
-		}
-		else if (times == 1)
-		{
-			color = 0;
-			times++;
-		}
-		else if (times == 2)
-		{
-			color = 165;
-			times++;
-		}
-	}
-	else if ((main_color == 1) || (main_color == -1))
-	{ 
-		if (times == 0)
-		{
-			color = 50;
-			times++;
-		}
-		else if (times == 1)
-		{
-			color = 0;
-			times++;
-		}
-		else if (times == 2)
-		{
-			color = 50;
-			times++;
-		}
-	}
-	else if ((main_color >= 2 && main_color <= 3) || (main_color <= -2 && main_color >= -3))
-	{
-		if (times == 0)
-		{
-			color = 100;
-			times++;
-		}
-		else if (times == 1)
-		{
-			color = 100;
-			times++;
-		}
-		else if (times == 2)
-		{
-			color = 0;
-			times++;
-		}
-	}
-	else if (main_color >= 3 && main_color <= 4)
-	{
-		if (times == 0)
-		{
-			color = 93;
-			times++;
-		}
-		else if (times == 1)
-		{
-			color = 51;
-			times++;
-		}
-		else if (times == 2)
-		{
-			color = 93;
-			times++;
-		}
-	}
-	else if (main_color >= 5 && main_color <= 6)
-	{
-		if (times == 0)
-		{
-			color = 255;
-			times++;
-		}
-		else if (times == 1)
-		{
-			color = 61;
-			times++;
-		}
-		else if (times == 2)
-		{
-			color = 1;
-			times++;
-		}
-	}
-	else if (main_color >= 7 && main_color <= 8)
-	{
-		if (times == 0)
-		{
-			color = 98;
-			times++;
-		}
-		else if (times == 1)
-		{
-			color = 50;
-			times++;
-		}
-		else if (times == 2)
-		{
-			color = 45;
-			times++;
-		}
-	}
-	else if (main_color >= 9)
-	{
-		if (times == 0)
-		{
-			color = 51;
-			times++;
-		}
-		else if (times == 1)
-		{
-			color = 255;
-			times++;
-		}
-		else if (times == 2)
-		{
-			color = 51;
-			times++;
-		}
-	}
+
+	if (times == 0)
+		color = 80;
+	else if (times == 1)
+		color = 0;
+	else if (times == 2)
+		color = 165;
+	return (color);
+}
+
+static int		get_top_color(int times)
+{
+	int		color;
+
+	if (times == 0)
+		color = 51;
+	else if (times == 1)
+		color = 255;
+	else if (times == 2)
+		color = 51;
+	return (color);
+}
+
+int				fdf_pxl_color(int main_color)
+{
+	int				color;
+	static int		times = 0;
+
+	if (main_color == 0 || (main_color == 1) || (main_color == -1)
+		|| (main_color >= 2 && main_color <= 3)
+		|| (main_color <= -2 && main_color >= -3)
+		|| (main_color >= 4 && main_color <= 5)
+		|| (main_color <= -4 && main_color >= -5))
+		color = get_minor_color(times++);
+	else if ((main_color >= 6 && main_color <= 7)
+		|| (main_color <= -6 && main_color >= -7)
+		|| (main_color >= 8 && main_color <= 9)
+		|| (main_color <= -6 && main_color >= -9))
+		color = get_middle_color(times++);
+	else if (main_color >= 10)
+		color = get_top_color(times++);
 	(times == 3) ? times = 0 : 0;
 	return (color);
 }
